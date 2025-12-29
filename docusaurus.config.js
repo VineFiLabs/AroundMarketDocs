07a1267b0c002c163555c050955e711deb09694c
whalehat <ouyangmin125@gmail.com>
2025-12-29 18:27:56 +0800

    feat: 🎸 update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,14 +20,14 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://aroundmarketdocs-yeaewjk2-quqicolour.ipfs.4everland.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'VineFi', // Usually your GitHub org/user name.
+  organizationName: 'VineFiLabs', // Usually your GitHub org/user name.
   projectName: 'Around Market', // Usually your repo name.
 
   onBrokenLinks: 'throw',
